e9c5ab6d0be2b7d989ec09a86891430b71cef78d
Børge Lundsaunet <mail@blund.site>
2024-12-19 00:13:40 +0100

    remove comments
    I don't want it to look like I had to use chatgpt to figure out how to
    do this (I did)

--- lua/api.c
+++ lua/api.c
@@ -4,44 +4,38 @@
 
 #include "../src/context.h"
 
-// Lua wrapper for cadence_setup
 int lua_cadence_setup(lua_State* L) {
-  int sample_rate = luaL_checkinteger(L, 1); // Get the sample_rate argument from Lua
-
-  // Call the original C function
+  int sample_rate = luaL_checkinteger(L, 1);
   cadence_ctx* ctx = cadence_setup(sample_rate);
 
-  // Push the context pointer as light userdata to Lua
   lua_pushlightuserdata(L, ctx);
-  return 1; // Return 1 value (the pointer)
+  return 1;
 }
 
-// Expose new_sine to Lua
 int lua_new_sine(lua_State* L) {
-  cadence_ctx* ctx = (cadence_ctx*)lua_touserdata(L, 1); // Get the context
-  sine_t* sine = new_sine(ctx); // Call C function to create a new sine object
+  cadence_ctx* ctx = (cadence_ctx*)lua_touserdata(L, 1);
+  sine_t* sine = new_sine(ctx);
 
-  lua_pushlightuserdata(L, sine); // Push the pointer as light userdata
-  return 1; // Return the pointer
+  lua_pushlightuserdata(L, sine);
+  return 1;
 }
 
 int lua_set_sine_freq(lua_State* L) {
-  sine_t* sine = (sine_t*)lua_touserdata(L, 1); // Get the sine pointer
-  float freq = luaL_checknumber(L, 2); // Get the context
+  sine_t* sine = (sine_t*)lua_touserdata(L, 1);
+  float freq = luaL_checknumber(L, 2);
   sine->freq = freq;
   return 0;
 }
 
-// Expose gen_sine to Lua
 int lua_gen_sine(lua_State* L) {
-  cadence_ctx* ctx = (cadence_ctx*)lua_touserdata(L, 1); // Get the context
-  sine_t* sine = (sine_t*)lua_touserdata(L, 2); // Get the sine pointer
+  cadence_ctx* ctx = (cadence_ctx*)lua_touserdata(L, 1);
+  sine_t* sine = (sine_t*)lua_touserdata(L, 2);
 
   if (!sine) return luaL_error(L, "Invalid sine pointer");
 
-  float sample = gen_sine(ctx, sine); // Call C function
-  lua_pushnumber(L, sample); // Push the result
-  return 1; // Return the sample
+  float sample = gen_sine(ctx, sine);
+  lua_pushnumber(L, sample);
+  return 1;
 }
 
 // Register functions in Lua
